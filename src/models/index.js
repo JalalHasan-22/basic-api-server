984e9703be806f8e9f3e4e0e9946690c3fae34a3
Jalal Hasan <97670683+JalalHasan-22@users.noreply.github.com>
2022-02-28 15:42:59 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -7,12 +7,12 @@ const food = require("./food.model");
 const POSTGRES_URL =
   process.env.DATABASE_URL || `postgresql://jalal:3713@localhost:5432/class03`;
 const sequelizeOptions = {
-  // dialectOptions: {
-  //   ssl: {
-  //     require: process.env.DATABASE_URL ? true : false,
-  //     rejectUnauthorized: false,
-  //   },
-  // },
+   dialectOptions: {
+     ssl: {
+       require: process.env.DATABASE_URL ? true : false,
+       rejectUnauthorized: false,
+     },
+   },
 };
 
 const sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
